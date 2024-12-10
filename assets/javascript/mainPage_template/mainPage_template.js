2bea985d41a158159ec3ffa51a2f769be1b35973
Arthurdnl <arthurdavisdnl001@gmail.com>
2024-12-10 20:55:46 -0300

    feat(arthur) - service

--- assets/javascript/mainPage_template/mainPage_template.js
+++ assets/javascript/mainPage_template/mainPage_template.js
@@ -22,3 +22,36 @@ for (let index = 0; index < sidebarLines.length; index++) {
         sidebarLines[index].classList.toggle("active");
     });
 };
+
+// main-services
+
+const corpo = document.querySelector("main")
+
+window.onload=()=>{
+    create_service()
+}
+
+function create_service(){
+    let divImg_user = document.createElement("div")
+    divImg_user.id = "divImg_user"
+    let img_user = document.createElement("img")
+    img_user.id= "img_user"
+    img_user.src = "../assets/images/user-icon-removebg-preview.svg"
+    divImg_user.appendChild(img_user)
+    let txt_user = document.createElement("h3")
+    txt_user.id = "txt_user"
+    txt_user.textContent = "Bem vindo" + " XXXXXX"
+    divImg_user.appendChild(txt_user)
+    corpo.appendChild(divImg_user)
+
+    let divBanner_service = document.createElement("div")
+    divBanner_service.id = "divBanner_service"
+    let banner_service = document.createElement("img")
+    banner_service.id = "banner_service"
+    banner_service.src = "../assets/images/frame 3.svg"
+    divBanner_service.appendChild(banner_service)
+    corpo.appendChild(divBanner_service)
+
+
+}
+
