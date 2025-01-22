fdcdb878331c8cc1418cc32f1415043b530d1fd6
sirius <siriusindra@gmail.com>
2025-01-22 15:41:13 -0300

    feat(gabriel) - contract edit buttons confirm and cancel

--- assets/javascript/indexContractScript/contract.js
+++ assets/javascript/indexContractScript/contract.js
@@ -1,8 +1,39 @@
 const editButton = document.querySelector(".button-edit");
 const textContractField = document.querySelector(".contract-text")
 
+const buttonsEditDiv = document.querySelector(".contract-edit-active");
+const confirmEditButton = document.querySelector(".confirm-edit");
+const cancelEditButton = document.querySelector(".cancel-edit");
+
 editButton.addEventListener("click", (event) =>{
 
+    buttonsEditDiv.style.display = "grid";
     textContractField.readOnly = false;
     textContractField.style.border = "solid #000 1px";
+});
+
+cancelEditButton.addEventListener("click", (event)=>{
+
+    var textContent = textContractField.textContent;
+
+    buttonsEditDiv.style.display = "none";
+
+    textContractField.readOnly = true;
+    textContractField.style.border = "none";
+    textContractField.textContent = textContent;
+});
+
+confirmEditButton.addEventListener("click", (event)=>{
+
+    var textContent = textContractField.textContent;
+
+    if(textContractField.textContent != textContent){
+
+        textContractField.textContent = textContractField.textContent;
+    }
+
+    buttonsEditDiv.style.display = "none";
+
+    textContractField.readOnly = true;
+    textContractField.style.border = "none";
 });
